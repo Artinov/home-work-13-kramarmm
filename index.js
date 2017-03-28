9b700fecb41565ec7c13f96e2a434a6eb8d9db33
kramarmm <kramar.mariya@mail.ru>
2017-03-28 09:11:36 +0300

    Add calculate() and process invalid input

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 var http = require('http');
 var port = 3000;
 
-	app.get('/favicon.ico', function(req, res) {
-    res.send(204);
-});
+
 var server = http.createServer(function(request, response) {
 
-    console.log(request.url);
-	if ( /[^\/\+\-\*\:\d]/g.test(toString(request.url)) ) {
-	    response.end("Only number need to calculate");
-	} else {
-	    switch (request.url) {
-	        case '/':
-	            response.end("Hello");
-	            break;
-	        default:
-	        	var number = request.url.slice(1);
-	        	parseInt(number,10);
-	        	response.end(number);
-	        	break;
-	    }
+    if (request.url !== '/favicon.ico') {
+
+        if (request.url === '/') {
+            response.end("Hello this is a url calculator!\nEnter two numbers afrer \/ in url.");
+        } else if (/[^\/\+\-\*\:\d]/g.test(request.url)) {
+            response.end("Only numbers can be calculate.\nPlease, enter a number.");
+        } else {
+            var number = request.url.slice(1);
+            var numberArr = number.split(/[\/\+\-\*\:]/g);
+            var signArr = number.split(/[^\/\+\-\*\:]*/g);
+            if (!numberArr[1]) {
+            	response.end("Please, enter second number.");
+            }
+            if (numberArr[2]||signArr[2]) {
+            	response.end("Sorry, but I can calculate only two numbers for a time.");
+            }
+            response.end("Success :)\n\n" + number + " = " + calculate(signArr[1], numberArr[0], numberArr[1]) + "");
+        }
+
+        function calculate(op, num1, num2) {
+            var result = 0;
+            var n1 = parseInt(num1);
+            var n2 = parseInt(num2);
+            switch (op) {
+                case "+":
+                    result = n1 + n2;
+                    break;
+                case "-":
+                    result = n1 - n2;
+                    break;
+                case "*":
+                    result = n1 * n2;
+                    break;
+                case ":":
+                    result = n1 / n2;
+                    break;
+            }
+            return result;
+        }
     }
 });
 
 server.listen(port, function() {
     console.log("Server is listening port: " + port);
-    console.log("Visit http://localhost:" + port + "to see your app");
+    console.log("Visit http://localhost:" + port + " to see your app");
 })
